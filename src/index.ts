d571c5c265107504c256c7acffc76d6a73d71845
Chester Millisock Jr <chester@millisock.com>
2022-01-05 12:43:07 -0800

    Change redirects base route to /r instead of /redirects

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(`${BASE_URL}/v1/messages`, routes.messages);
-app.use(`${BASE_URL}/v1/redirects`, routes.redirects);
+app.use(`${BASE_URL}/v1/r`, routes.redirects);
 
 app.listen(port, '0.0.0.0', () => {
     console.log(`Listening on port: ${port}`);
